8e1e252a92e220b35fd40cc85fd8dfd9edbf60a0
anand <anandtyagi77@gmail.com>
2018-05-05 18:38:41 +0530

    Routing integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,24 @@ import {FormsModule} from "@angular/forms";
 import {ConvertToSpacePipe} from "./shared/convert-to-spaces.pipe";
 import {StarComponent} from "./shared/star.component";
 import {HttpClientModule} from "@angular/common/http";
-import { ProductDetailComponent } from './product/product-detail.component';
+import {ProductDetailComponent} from './product/product-detail.component';
 import {WelcomeComponent} from "./home/welcome.component";
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent, ConvertToSpacePipe,StarComponent, ProductDetailComponent,WelcomeComponent
+    AppComponent, ProductListComponent, ConvertToSpacePipe, StarComponent, ProductDetailComponent, WelcomeComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(
+      [
+        {path: 'welcome', component: WelcomeComponent},
+        {path: 'products', component: ProductListComponent},
+        {path: 'product/:id', component: ProductDetailComponent},
+        {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+        {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
